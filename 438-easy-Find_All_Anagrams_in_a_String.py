abd653e6193da769c02b028a53b7c68a3c9e96e0
Yuan Hao <pearl1643@live.com>
2018-07-16 22:31:59 +0800

    E14-438-2

--- 438-easy-Find_All_Anagrams_in_a_String.py
+++ 438-easy-Find_All_Anagrams_in_a_String.py
@@ -9,27 +9,32 @@ class Solution:
         if len(s)<len(p) :
             return []
 
-        # We use two dictionary dp/dt and ds, dp is initialized with all values are 1.
-        # If we find a match charactor, the value of corresponding key-value pair in dp would be set to 0.
+        # We use two dictionary dp/dt and ds, dp is initialized with all values equal to the amount of a alphabet.
+        # If we find a match charactor, the value of corresponding key-value pair in dp would decrease by 1.
         # If after len(p) steps, all the values in dp are 0, put corresponding index of s in result.
 
         dp = {}
+
         for i in range(len(p)) :
-            dp[p[i]] = int(1)
+            dp[p[i]] = int(0)
+
+        for i in range(len(p)) :
+            dp[p[i]] += 1
 
         results = []
 
         for j in range(0, len(s)-len(p)+1):
             if s[j] in dp:
                 dt = dp.copy()
-                dt[s[j]] = 0 # cross that charactor in this run
+                dt[s[j]] -= 1 # cross that charactor in this run
 
                 t = j + 1
 
                 for k in range(len(p) - 1):
-                    if s[t] in dt and dt[s[t]] == 1: # another charactor match and it has not been crossed
-                        dt[s[t]] = 0
+                    if s[t] in dt and dt[s[t]] != 0: # another charactor match and it has not been crossed
+                        dt[s[t]] -= 1
                         t += 1
+
 
                 if t - j == len(p): # successfully cross len(p) charactors
                     results.append(j)
@@ -40,4 +45,4 @@ class Solution:
 
                         
 if __name__ == '__main__':
-    print(Solution().findAnagrams('cbaebabacd', 'abc'))
+    print(Solution().findAnagrams('baa', 'aa'))
