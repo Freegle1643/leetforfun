f4211c79c7adba36214b665dd939aac195aa2a9d
Yuan Hao <pearl1643@live.com>
2018-07-28 22:32:23 +0800

    E26-416 Explanation

--- 416-mid-Partition_Equal_Subset_Sum-180728.py
+++ 416-mid-Partition_Equal_Subset_Sum-180728.py
@@ -26,6 +26,14 @@ Thanks to ZhuEason
 
 https://leetcode.com/problems/partition-equal-subset-sum/discuss/90592/01-knapsack-detailed-explanation/159315
 
+1. dp[i][j] means whether the specific sum j can be gotten from the first i numbers. If we can pick such a series 
+	of numbers from 0-i whose sum is j, dp[i][j] is true, otherwise it is false.
+2. For each number, if we don't pick it, dp[i][j] = dp[i-1][j], which means if the first i-1 elements has made it 
+	to j, dp[i][j] would also make it to j (we can just ignore nums[i]). If we pick nums[i]. dp[i][j] = dp[i-1][j-nums[i]], 
+	which represents that j is composed of the current value nums[i] and the remaining composed of other previous numbers. 
+	Thus, the transition function is dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+
+
 """
 
 # Result TLE 104/104 passed
