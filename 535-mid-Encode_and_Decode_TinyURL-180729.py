caaaf8c811671dc08c5b029a1dc382e365f17cc4
Yuan Hao <pearl1643@live.com>
2018-07-29 21:10:01 +0800

    E27-535

--- 535-mid-Encode_and_Decode_TinyURL-180729.py
+++ 535-mid-Encode_and_Decode_TinyURL-180729.py
@@ -10,15 +10,53 @@ Design the encode and decode methods for the TinyURL service. There is no restri
 
 """
 
-class Codec:
+# Result AC 100 % 24ms
 
+# class Codec:
+
+#     def encode(self, longUrl):
+#         return longUrl
+
+#     def decode(self, shortUrl):
+#         return shortUrl
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+
+
+# Result 28ms 82.46%
+
+class Codec:
+    import string
+    letters = string.ascii_letters + string.digits
+    full_tiny = {}
+    tiny_full = {}
+    global_counter = 0
     def encode(self, longUrl):
         """Encodes a URL to a shortened URL.
         
         :type longUrl: str
         :rtype: str
         """
-        return longUrl
+        def decto62(dec):
+            ans = ""
+            while 1:
+                ans = self.letters[dec % 62] + ans
+                dec //= 62
+                if not dec:
+                    break
+            return ans
+                
+        suffix = decto62(self.global_counter)
+        if longUrl not in self.full_tiny:
+            self.full_tiny[longUrl] = suffix
+            self.tiny_full[suffix] = longUrl
+            self.global_counter += 1
+        return "http://tinyurl.com/" + suffix
+        
+        
 
     def decode(self, shortUrl):
         """Decodes a shortened URL to its original URL.
@@ -26,8 +64,16 @@ class Codec:
         :type shortUrl: str
         :rtype: str
         """
-        return shortUrl
+        idx = shortUrl.split('/')[-1]
+        if idx in self.tiny_full:
+            return self.tiny_full[idx]
+        else:
+            return None
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
+
+if __name__ == '__main__':
+    from time import time
+    codec = Codec()
+    t = time()
+    ans = codec.decode(codec.encode("https://leetcode.com/problems/design-tinyurl"))
+    print('ans: %s\ntime: %.3fms' % (ans, (time() - t) * 1000))             
