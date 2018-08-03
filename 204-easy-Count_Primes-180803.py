de7c649b45ead7cfd9eb04a34a1736256a48ca5c
Yuan Hao <pearl1643@live.com>
2018-08-03 13:35:43 +0800

    E32-204

---
+++ 204-easy-Count_Primes-180803.py
@@ -0,0 +1,43 @@
+"""
+204. Count Primes
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+"""
+
+# Resutl AC 488ms 60.45%
+
+
+class Solution:
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n -= 1
+       	if n < 2:
+       		return 0
+
+       	marker = [1] * (n+1)
+       	marker[0] = marker[1] = 0
+
+       	for i in range(2, int(n ** 0.5) + 1):
+       		if marker[i]:
+       			for j in range(i + i, n + 1, i):
+       				marker[j] = 0
+
+       	return sum(marker)
+
+
+if __name__ == '__main__':
+	sol = Solution()
+	from time import time
+	t = time()
+	ans = sol.countPrimes(10)
+	print('ans: %s\ntime: %.3fms' % (ans, ((time() - t)) * 1000))  
