060c917167910429e26fd86cab0ee89dbadd80ca
Yuan Hao <pearl1643@live.com>
2018-08-07 17:47:23 +0800

    E36-224

---
+++ 224-hard-Basic_Calculator-180807.py
@@ -0,0 +1,52 @@
+"""
+224. Basic Calculator
+
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+Example 1:
+
+Input: "1 + 1"
+Output: 2
+Example 2:
+
+Input: " 2-1 + 2 "
+Output: 3
+Example 3:
+
+Input: "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+Note:
+You may assume that the given expression is always valid.
+Do not use the eval built-in library function.
+
+eval() exceeds mem limit
+https://leetcode.com/problems/basic-calculator/discuss/62344/Easy-18-lines-C++-16-lines-Python
+
+"""
+
+
+class Solution:
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # return eval(s)
+        total = 0
+        i, signs = 0, [1, 1]
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                start = i
+                while i < len(s) and s[i].isdigit():
+                    i += 1
+                total += signs.pop() * int(s[start:i])
+                continue
+            if c in '+-(':
+                signs += signs[-1] * (1, -1)[c == '-'],
+            elif c == ')':
+                signs.pop()
+            i += 1
+        return total
