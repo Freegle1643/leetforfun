13a732d48bb0f37be5842f8650fcb264143004f3
Yuan Hao <pearl1643@live.com>
2018-08-13 18:00:30 +0800

    E42-647

---
+++ 647-mid-Palindromic_Substrings-180813.py
@@ -0,0 +1,38 @@
+"""
+647. Palindromic Substrings
+
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+
+# Note!!!
+You are requested to determine how many, instead of True or False
+
+"""
+
+# Result AC 768ms 13.12%
+
+
+class Solution:
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    res += 1
+        return res
