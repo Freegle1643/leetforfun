68db17cc291127ad464fc90a2710004131754d86
freegle1643 <pearl1643@live.com>
2019-01-06 21:08:11 -0800

    S02-215

---
+++ 215-mid-Kth_Largest_Element_in_an_Array-190105.py
@@ -0,0 +1,39 @@
+"""
+215. Kth Largest Element in an Array
+
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+"""
+
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        """
+        using minheap
+        """
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for i in range(k, len(nums)):
+            if nums[i] > heap[0]:
+                heapq.heapreplace(heap, nums[i])
+        return heap[0]
+        """
+        using python lib
+        """
+        # return sorted(nums)[-k]
